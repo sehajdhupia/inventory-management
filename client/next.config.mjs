fd3dc3949de1aae2581056ffc37ef0107a9fabfa
Sehaj Dhupia <sehaj.dhupia@gmail.com>
2024-09-14 23:44:50 -0400

    added images again

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "https://s3-sehajinventorymanagement.s3.us-east-2.amazonaws.com",
+                hostname: "s3-sehajinventorymanagement.s3.us-east-2.amazonaws.com",
                 port: "",
                 pathname: "/**",
             },
